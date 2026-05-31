872eb37510b2640ab0978c6f0252598659623429
rayssamell <rayssamelltpe@gmail.com>
2026-05-31 11:10:55 -0300

    correção no código

--- cache_def.sv
+++ cache_def.sv
@@ -1,52 +1,52 @@
 package cache_def;
-// data structures for cache tag & data
-parameter int TAGMSB = 31; //tag msb
-parameter int TAGLSB = 14; //tag lsb
+    // data structures for cache tag & data
+    parameter int TAGMSB = 31; //tag msb
+    parameter int TAGLSB = 14; //tag lsb
 
-//data structure for cache tag
-typedef struct packed {
- bit valid; //valid bit
- bit dirty; //dirty bit
- bit [TAGMSB:TAGLSB]tag; //tag bits
-} cache_tag_type;
+    //data structure for cache tag
+    typedef struct packed {
+        logic valid; //valid logic
+        logic dirty; //dirty logic
+        logic [TAGMSB:TAGLSB]tag; //tag logics
+    } cache_tag_type;
 
-//data structure for cache memory request
-typedef struct {
- bit [9:0]index; //10-bit index
- bit we; //write enable
-} cache_req_type;
+    //data structure for cache memory request
+    typedef struct packed{
+        logic [9:0]index; //10-logic index
+        logic we; //write enable
+    } cache_req_type;
 
-//128-bit cache line data
-typedef bit [127:0] cache_data_type;
+    //128-logic cache line data
+    typedef logic [127:0] cache_data_type;
 
-// data structures for CPU<->Cache controller interface
-// CPU request (CPU->cache controller)
-typedef struct {
- bit [31:0]addr; //32-bit request addr
- bit [31:0]data; //32-bit request data (used when write)
- bit rw; //request type : 0 = read, 1 = write
- bit valid; //request is valid
-} cpu_req_type;
+    // data structures for CPU<->Cache controller interface
+    // CPU request (CPU->cache controller)
+    typedef struct packed {
+        logic [31:0]addr; //32-logic request addr
+        logic [31:0]data; //32-logic request data (used when write)
+        logic rw; //request type : 0 = read, 1 = write
+        logic valid; //request is valid
+    } cpu_req_type;
 
-// Cache result (cache controller->cpu)
-typedef struct {
- bit [31:0]data; //32-bit data
- bit ready; //result is ready
-} cpu_result_type;
+    // Cache result (cache controller->cpu)
+    typedef struct packed {
+        logic [31:0]data; //32-logic data
+        logic ready; //result is ready
+    } cpu_result_type;
 
-//----------------------------------------------------------------------
-// data structures for cache controller<->memory interface
-// memory request (cache controller->memory)
-typedef struct {
- bit [31:0]addr; //request byte addr
- bit [127:0]data; //128-bit request data (used when write)
- bit rw; //request type : 0 = read, 1 = write
- bit valid; //request is valid
-} mem_req_type;
+    //----------------------------------------------------------------------
+    // data structures for cache controller<->memory interface
+    // memory request (cache controller->memory)
+    typedef struct packed {
+        logic [31:0]addr; //request byte addr
+        logic [127:0]data; //128-logic request data (used when write)
+        logic rw; //request type : 0 = read, 1 = write
+        logic valid; //request is valid
+    } mem_req_type;
 
-// memory controller response (memory -> cache controller)
-typedef struct {
- cache_data_type data; //128-bit read back data
- bit ready; //data is ready
-} mem_data_type;
+    // memory controller response (memory -> cache controller)
+    typedef struct packed {
+        cache_data_type data; //128-logic read back data
+        logic ready; //data is ready
+    } mem_data_type;
 endpackage
--- dm_cache_data.sv
+++ dm_cache_data.sv
@@ -2,24 +2,25 @@ import cache_def::*;
 
 /*cache: data memory, single port, 1024 blocks*/
 module dm_cache_data(
- input bit clk,
- input cache_req_type data_req,//data request/command, e.g. RW, valid
- input cache_data_type data_write, //write port (128-bit line)
- output cache_data_type data_read //read port
+  input logic clk,
+  input cache_req_type data_req,//data request/command, e.g. RW, valid
+  input cache_data_type data_write, //write port (128-bit line)
+  output cache_data_type data_read //read port
 );
 timeunit 1ns; timeprecision 1ps;
 
 cache_data_type data_mem[0:1023];
+integer i;
 
 initial begin
- for (int i=0; i<1024; i++)
-  data_mem[i] = '0;
-end
+  for (i=0; i<1024; i=i+1)
+    data_mem[i] = '0;
+  end
 
-assign data_read = data_mem[data_req.index];
+  assign data_read = data_mem[data_req.index];
 
-always_ff @(posedge(clk)) begin
- if (data_req.we)
-  data_mem[data_req.index] <= data_write;
-end
+  always_ff @(posedge(clk)) begin
+  if (data_req.we)
+    data_mem[data_req.index] <= data_write;
+  end
 endmodule
